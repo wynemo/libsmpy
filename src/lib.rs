d58c2c770f96664daf40595a3f105c326d25482f
Tommy Green <tommygreen@Tommys-Mac-mini.local>
2023-07-02 22:31:31 +0800

    ✨: add sm4_encrypt, sm4_decrypt function

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,6 @@ use pyo3::exceptions::PyOSError;
 use libsm::sm2::signature::SigCtx;
 use libsm::sm2::encrypt::{EncryptCtx, DecryptCtx};
 
-// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-   Ok((a + b).to_string())
-}
-
-// struct MyOtherError(FromHexError);
-
-// impl From<MyOtherError> for PyErr {
-//     fn from(error: MyOtherError) -> Self {
-//         PyValueError::new_err(error.0.to_string())
-//     }
-// }
-
-// impl From<FromHexError> for MyOtherError {
-//     fn from(other: FromHexError) -> Self {
-//         Self(other)
-//     }
-// }
-
 #[pyclass]
 struct Keypair {
     pub pri_hex: String,
@@ -61,29 +41,40 @@ impl Keypair {
     }
 
     pub fn encrypt(&self, msg: String) -> PyResult<String> {
-        let ctx = SigCtx::new();
         let s = self.pub_hex.to_string();
-        let pub_k = hex::decode(s).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode hex"))?; 
-        let pk_raw = ctx.load_pubkey(&pub_k).map_err(|_| PyErr::new::<PyOSError, _>("Failed to load public key"))?;
-        let b_msg = msg.as_bytes();
-        let klen = b_msg.len();
-        let encrypt_ctx = EncryptCtx::new(klen, pk_raw);
-        let cipher = encrypt_ctx.encrypt(b_msg).unwrap();
-        Ok(hex::encode(cipher))
+        sm4_encrypt(s, msg)
     }
 
     pub fn decrypt(&self, hex_msg: String) -> PyResult<String> {
-        let ctx = SigCtx::new();
         let s = self.pri_hex.to_string();
-        let private_k = hex::decode(s).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode hex"))?; 
-        let private_raw = ctx.load_seckey(&private_k).map_err(|_| PyErr::new::<PyOSError, _>("Failed to load private key"))?;
-        let msg = hex::decode(hex_msg).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode msg hex"))?; 
-        let klen = msg.len() - 97;
-        println!("klen is {}", klen);
-        let decrtyp_ctx = DecryptCtx::new(klen, private_raw);
-        let plain = decrtyp_ctx.decrypt(&msg).unwrap();
-        Ok(String::from_utf8_lossy(&plain).to_string())
+        sm4_decrypt(s, hex_msg)
     }
+}
+
+#[pyfunction]
+fn sm4_decrypt(prviate_key: String, hex_msg: String) -> PyResult<String> {
+    let ctx = SigCtx::new();
+    let private_k = hex::decode(prviate_key).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode hex"))?; 
+    let private_raw = ctx.load_seckey(&private_k).map_err(|_| PyErr::new::<PyOSError, _>("Failed to load private key"))?;
+    let msg = hex::decode(hex_msg).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode msg hex"))?; 
+    let klen = msg.len() - 97;
+    println!("klen is {}", klen);
+    let decrtyp_ctx = DecryptCtx::new(klen, private_raw);
+    let plain = decrtyp_ctx.decrypt(&msg).unwrap();
+    Ok(String::from_utf8_lossy(&plain).to_string())
+}
+
+#[pyfunction]
+fn sm4_encrypt(public_key: String, msg: String) -> PyResult<String> {
+    let ctx = SigCtx::new();
+    let s = public_key.to_string();
+    let pub_k = hex::decode(s).map_err(|_| PyErr::new::<PyOSError, _>("Failed to decode hex"))?; 
+    let pk_raw = ctx.load_pubkey(&pub_k).map_err(|_| PyErr::new::<PyOSError, _>("Failed to load public key"))?;
+    let b_msg = msg.as_bytes();
+    let klen = b_msg.len();
+    let encrypt_ctx = EncryptCtx::new(klen, pk_raw);
+    let cipher = encrypt_ctx.encrypt(b_msg).unwrap();
+    Ok(hex::encode(cipher))
 }
 
 
@@ -92,7 +83,8 @@ impl Keypair {
 /// import the module.
 #[pymodule]
 fn libsmpy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sm4_decrypt, m)?)?;
+    m.add_function(wrap_pyfunction!(sm4_encrypt, m)?)?;
     m.add_class::<Keypair>()?;
     Ok(())
 }
